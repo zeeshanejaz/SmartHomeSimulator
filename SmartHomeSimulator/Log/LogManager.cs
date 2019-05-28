dfbca41befe88f43370123622c67b456d1b8c482
Zeeshan Ejaz Bhatti <zeeshanejaz@gmail.com>
2019-05-28 19:44:17 +1200

    Added missing files and updated gitignore

---
+++ SmartHomeSimulator/Log/LogManager.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHomeSimulator.Log
+{
+    internal class LogManager
+    {
+        public static void LogMessage(string message)
+        {
+            ViewForm.Instance.AddLog(message);
+        }
+    }
+}
---
+++ SmartHomeSimulator/Log/LoggedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmartHomeSimulator.Log
+{
+    internal class LoggedException : Exception
+    {
+        public LoggedException(string message)
+        : base(message)
+        {
+        }
+
+        public LoggedException(string format, params object[] parameters)
+            : base(string.Format(format, parameters))
+        {
+        }
+    }
+}
